96751ed043b45aa160605335d5ff73a3d8ce68e8
balagrivine <130163941+balagrivine@users.noreply.github.com>
2023-06-03 08:30:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-command = " "
-started = True
-while command != "quit":
-    msg = input(">>>")
-    if msg == "start":
-        if started:
-           print("Car started what are you doing!")
-        else:
-            print("car started...")
-    elif msg == "stop":
-        if not started:
-            print("Car stopped what are you doing")
-        else:
-            started = False
-            print("car stopped...")
-    elif msg == "help":
-        print("""
-        start -to start the car
-        stop -to stop the car
-        quit -to exit""")
-    else:
-        print("sorry. I don't understand that!")
+balance = 2000000
+command = input("Perform action >  ")
+if command.lower() == "withdraw":
+    amount_to_withdraw = int(input("Enter amount >  "))
+    while amount_to_withdraw < balance:
+        pin = int(input("Enter pin >  "))
+        max_attempts = 3
+        if pin == 1234:
+            print("Successfully withdrawn" + " " + str(amount_to_withdraw))
+            print("New account balance is" + " " + str(balance-amount_to_withdraw))
+            break
+        if pin != 1234:
+            max_attempts += 1
+            print("Sorry, you entered the wrong pin")
+    if amount_to_withdraw > balance:
+        print("Sorry, the amount you are trying to withdraw is more than what's in your account.")
+
+elif command.lower() == "deposit":
+    amount_to_deposit = int(input("Enter amount >  "))
+    prompt = input("Do you want to check balance?  ")
+    if prompt == "yes":
+        balance = balance + amount_to_deposit
+        print("Your balance is" + " " + "Ksh." + " " + str(balance))
+
